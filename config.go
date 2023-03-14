fa8c2f35a457144ee76298f266d856c91f9d9f22
Gabriel Loyola Daiqui <git@loyola.dev>
2023-03-14 22:52:47 +0100

    Checks for undefined config keys

--- config.go
+++ config.go
@@ -23,13 +23,18 @@ func readConfig() (*GitlabConfig, error) {
 
 	// Load config
 	config := GitlabConfig{}
-	_, err = toml.DecodeFile(path.Join(usr.HomeDir, ".gn.toml"), &config)
+	metaData, err := toml.DecodeFile(path.Join(usr.HomeDir, ".gn.toml"), &config)
 	if err != nil {
 		if os.IsNotExist(err) {
-			return nil, fmt.Errorf("the config file at \"~/.gn.toml\" does not exist")
+			return nil, fmt.Errorf("config file at \"~/.gn.toml\" does not exist")
 		}
 
 		return nil, fmt.Errorf("could not decode config: %w", err)
+	}
+
+	// Check if the config only contains the keys we expect
+	if len(metaData.Undecoded()) > 0 {
+		return nil, fmt.Errorf("config contains unexpected keys: %+v", metaData.Undecoded())
 	}
 
 	return &config, nil
