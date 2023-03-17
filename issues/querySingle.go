c5a9b41af532c585de529a4749252cb9e54be853
Gabriel Loyola Daiqui <git@loyola.dev>
2023-03-17 21:16:45 +0100

    QuerySingle returns IssueDetail struct

--- issues/querySingle.go
+++ issues/querySingle.go
@@ -1,36 +1,93 @@
 package issues
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
+	"time"
 
 	"gn/config"
 	"gn/requests"
 )
 
-func QuerySingle(config *config.Gitlab, projectPath string, issueID string) {
+type querySingleResponse struct {
+	Data struct {
+		Project struct {
+			Issue struct {
+				Title       string    `json:"title"`
+				Description string    `json:"description"`
+				CreatedAt   time.Time `json:"createdAt"`
+				UpdatedAt   time.Time `json:"updatedAt"`
+				Assignees   struct {
+					Nodes []User `json:"nodes"`
+				} `json:"assignees"`
+				Labels struct {
+					Nodes []struct {
+						Title string `json:"title"`
+					} `json:"nodes"`
+				} `json:"labels"`
+				Discussions struct {
+					Nodes []struct {
+						Notes struct {
+							Nodes []struct {
+								System       bool
+								User         `json:"author"`
+								Body         string      `json:"body"`
+								CreatedAt    time.Time   `json:"createdAt"`
+								UpdatedAt    time.Time   `json:"updatedAt"`
+								Resolved     bool        `json:"resolved"`
+								LastEditedBy interface{} `json:"lastEditedBy"`
+							} `json:"nodes"`
+						} `json:"notes"`
+					} `json:"nodes"`
+				} `json:"discussions"`
+			} `json:"issue"`
+		} `json:"project"`
+	} `json:"data"`
+}
+
+func QuerySingle(config *config.Gitlab, projectPath string, issueID string) (*IssueDetails, error) {
 	query := `
 		query($projectPath: ID!, $issueIID: String!) {
 		  project(fullPath: $projectPath) {
-		    issue(iid: $issueIID) {
-		      title
-		      description
-		      createdAt
-		      updatedAt
-		      webUrl
-		      discussions {
-		        nodes {
-		          id
-		          notes {
-		            nodes {
-		              body
-		              createdAt
-		              updatedAt
-		            }
-		          }
-		        }
-		      }
-		    }
+			issue(iid: $issueIID) {
+			  title
+			  description
+			  createdAt
+			  updatedAt
+			  assignees {
+				nodes {
+				  name
+				  username
+				}
+			  }
+			  labels {
+				nodes {
+				  title
+				}
+			  }
+			  discussions {
+				nodes {
+				  notes {
+					nodes {
+					  system
+					  author {
+						name
+						username
+					  }
+					  body
+					  createdAt
+					  updatedAt
+					  resolved
+					  lastEditedBy {
+						name
+						username
+					  }
+					}
+				  }
+				}
+			  }
+			}
 		  }
 		}
 	`
@@ -46,8 +103,83 @@ func QuerySingle(config *config.Gitlab, projectPath string, issueID string) {
 	}, config)
 
 	if err != nil {
-		log.Fatalf("query single: %s\n", err)
+		return nil, fmt.Errorf("query single - request failed: %w", err)
 	}
 
-	fmt.Printf("Single issue:\n%s\n", response)
+	querySingle := querySingleResponse{}
+	err = json.Unmarshal(response, &querySingle)
+	if err != nil {
+		return nil, fmt.Errorf("unmarshal of issues failed: %w", err)
+	}
+
+	issueDetails := IssueDetails{
+		Title:       querySingle.Data.Project.Issue.Title,
+		Description: querySingle.Data.Project.Issue.Description,
+		CreatedAt:   querySingle.Data.Project.Issue.CreatedAt,
+		UpdatedAt:   querySingle.Data.Project.Issue.UpdatedAt,
+		Assignees:   nil,
+		Labels:      nil,
+		Discussion:  nil,
+	}
+
+	// Flatten response
+	// Assignees
+	assignees := make([]User, 0)
+	for _, assignee := range querySingle.Data.Project.Issue.Assignees.Nodes {
+		assignees = append(assignees, User{
+			Name:     assignee.Name,
+			Username: assignee.Username,
+		})
+	}
+	issueDetails.Assignees = assignees
+
+	// Labels
+	labels := make([]string, 0)
+	for _, label := range querySingle.Data.Project.Issue.Labels.Nodes {
+		labels = append(labels, label.Title)
+	}
+	issueDetails.Labels = labels
+
+	// Discussion
+	for _, node := range querySingle.Data.Project.Issue.Discussions.Nodes {
+		inner := node.Notes.Nodes
+		if len(inner) == 0 {
+			log.Printf("query single - discussion without nodes?\n%s", response)
+			continue
+		}
+
+		if inner[0].System == true {
+			continue
+		}
+
+		comment := Comment{
+			Author: User{
+				Name:     inner[0].Name,
+				Username: inner[0].Username,
+			},
+			Body:         inner[0].Body,
+			CreatedAt:    inner[0].CreatedAt,
+			UpdatedAt:    inner[0].UpdatedAt,
+			Resolved:     inner[0].Resolved,
+			LastEditedBy: inner[0].LastEditedBy,
+			Comments:     make([]Comment, 0),
+		}
+
+		// Get sub comments
+		for _, subComment := range inner[1:] {
+			comment.Comments = append(comment.Comments, Comment{
+				Author:       subComment.User,
+				Body:         subComment.Body,
+				CreatedAt:    subComment.CreatedAt,
+				UpdatedAt:    subComment.UpdatedAt,
+				Resolved:     subComment.Resolved,
+				LastEditedBy: subComment.LastEditedBy,
+				Comments:     nil,
+			})
+		}
+
+		issueDetails.Discussion = append(issueDetails.Discussion, comment)
+	}
+
+	return &issueDetails, nil
 }
--- issues/struct.go
+++ issues/struct.go
@@ -11,3 +11,28 @@ type Issue struct {
 	State       string    `json:"state"`
 	Assignees   []string  `json:"assignees"`
 }
+
+type IssueDetails struct {
+	Title       string    `json:"title"`
+	Description string    `json:"description"`
+	CreatedAt   time.Time `json:"createdAt"`
+	UpdatedAt   time.Time `json:"updatedAt"`
+	Assignees   []User    `json:"assignees"`
+	Labels      []string  `json:"labels"`
+	Discussion  []Comment `json:"discussion"`
+}
+
+type Comment struct {
+	Author       User        `json:"author"`
+	Body         string      `json:"body"`
+	CreatedAt    time.Time   `json:"createdAt"`
+	UpdatedAt    time.Time   `json:"updatedAt"`
+	Resolved     bool        `json:"resolved"`
+	LastEditedBy interface{} `json:"lastEditedBy"` // ToDo: Remove interface
+	Comments     []Comment   `json:"comments"`
+}
+
+type User struct {
+	Name     string `json:"name"`
+	Username string `json:"username"`
+}
