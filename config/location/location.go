f53c1babdae1c1ac0d957c1fd5c1b289da63002c
Gabriel Loyola Daiqui <git@loyola.dev>
2023-08-14 10:07:26 +0200

    Config location now based on $XDG_CONFIG_HOME

--- config/location/location.go
+++ config/location/location.go
@@ -7,8 +7,15 @@ import (
 	"path"
 )
 
-// Dir returns the path to the configuration directory.
+// Dir returns the path to the configuration directory. If $XDG_CONFIG_HOME is set, $XDG_CONFIG_HOME/gin is used.
+// Otherwise, $HOME/.config/gin is used.
 func Dir() (string, error) {
+	// Check if $XDG_CONFIG_HOME is set
+	val, ok := os.LookupEnv("XDG_CONFIG_HOME")
+	if ok && len(val) > 0 {
+		return path.Join(val, "gin"), nil
+	}
+
 	// Get the user's home directory
 	usr, err := user.Current()
 	if err != nil {
