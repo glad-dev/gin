e39915f1d15db1f44007f3f71c94ed027070e757
Gabriel Loyola Daiqui <git@loyola.dev>
2024-02-28 10:23:53 +0100

    Starts mock for Gitlab's API

--- configuration/main_test.go
+++ configuration/main_test.go
@@ -1,9 +1,12 @@
 package configuration
 
 import (
+	"fmt"
 	"log"
+	"net/http"
 	"os"
 	"testing"
+	"time"
 )
 
 func TestMain(m *testing.M) {
@@ -18,4 +21,30 @@ func TestMain(m *testing.M) {
 	}
 
 	os.Exit(m.Run())
+}
+
+func mockGitlabAPI() {
+	http.HandleFunc("/api/v4/personal_access_tokens/self", func(w http.ResponseWriter, r *http.Request) {
+		// Set yesterday as "created_at" date
+		createdAt := time.Now().AddDate(0, 0, -1).Format("2006-01-02T15:04:05.000Z")
+		expiresAt := time.Now().AddDate(0, 0, 1).Format("2006-01-02")
+
+		_, _ = fmt.Fprintf(w, ""+
+			"{"+
+			"\"id\": 123,"+
+			"\"name\": \"Test token\","+
+			"\"revoked\": false,"+
+			"\"created_at\": \"%s\","+
+			"\"scopes\": [\"read_api\",\"read_user\",\"read_repository\"],"+
+			"\"user_id\": 456,"+
+			"\"active\": true,"+
+			"\"expires_at\": \"%s\""+
+			"}", createdAt, expiresAt,
+		)
+	})
+	
+	err := http.ListenAndServe(":3333", nil)
+	if err != nil {
+		log.Fatalf("Failed to run server: %s", err)
+	}
 }
